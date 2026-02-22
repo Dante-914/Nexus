a5fd7e0fbc166e78e925d1b26ab0ddbbe5ae2a88
Dante-914 <daniboi001@gmail.com>
2026-02-22 23:37:40 +0100

    Fix: Add Vite dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://xexus.onrender.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  plugins: [react()],
 })
